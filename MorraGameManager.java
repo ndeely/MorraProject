1d5832233b279323c6f12e556d5dbfd64c27627b
gitlassian <nialldeely@gmail.com>
2020-03-16 15:12:44 +0000

    added "this" when using code from outside function

--- MorraGameManager.java
+++ MorraGameManager.java
@@ -10,9 +10,9 @@ public class MorraGameManager {
 
 	public MorraGameManager() {
 		while(!play.equals("n")) {
-			String alreadyPlayed = (games.size()>0)?"another":"a"; //if this isn't the first game, ask if they want to play "another" one
+			String alreadyPlayed = (this.games.size()>0)?"another":"a"; //if this isn't the first game, ask if they want to play "another" one
 			System.out.println("Would you like to play " + alreadyPlayed + " game of Morra? (y/n)");
-			play = sc.next().substring(0,1).toLowerCase();
+			play = this.sc.next().substring(0,1).toLowerCase();
 			if(play.equals("y")) {
 				playGame();
 			} else if (play.equals("n")) { //if they don't want to continue, print out their game summary
